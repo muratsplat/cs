e7dab2ade447cf15ebc164e5d799fba0961da95e
Murat Ödünç <murat.asya@gmail.com>
2015-11-06 13:35:16 +0300

    adds new files about http request for the api

---
+++ app/Libs/ClearSettle/Resource/Request/Login.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Libs\ClearSettle\Request;
+
+//use Exception;
+
+
+/**
+ * Login Request
+ * 
+ * This class to sends  login request Clear Settle Api..
+ *
+ * @author Murat Ödünç <murat.asya@gmail.com>
+ */
+Class Login  extends Request
+{   
+    
+      
+}
---
+++ app/Libs/ClearSettle/Resource/Request/Request.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Libs\ClearSettle\Request;
+
+use Exception;
+use GuzzleHttp\Client;
+use App\Libs\ClearSettle\User;
+use Illuminate\Support\MessageBag;
+use Illuminate\Contracts\Support\MessageProvider;
+
+/**
+ * Abstract Request
+ * 
+ * This class  is based to sends request Clear Settle Api..
+ *
+ * @author Murat Ödünç <murat.asya@gmail.com>
+ */
+Abstract Class Request implements MessageProvider
+{   
+    
+    /**
+     * User Attributes
+     * @var array
+     */
+    protected $user;
+    
+    /**
+     * Guzzle Http Pre-Configured Client
+     *
+     * @var \GuzzleHttp\Client
+     */
+    protected $client;
+    
+    /**
+     * Message Bag
+     *
+     * @var \Illuminate\Support\MessageBag
+     */
+    protected $messageBag;
+
+        /**
+         * Create Instance
+         * 
+         * @param \App\Libs\ClearSettle\User    $user
+         * @param G\uzzleHttp\Client            $client
+         */
+        public function __construct(User $user, Client $client) 
+        {
+            $this->user         = $user;
+            
+            $this->client       = $client;
+            
+            $this->messageBag   = new MessageBag();
+        }
+        
+        
+        /**
+         * To get Message Bag
+         * 
+         * @return Illuminate\Contracts\Support\MessageProvider
+         */
+        public function getMessageBag() 
+        {            
+            return $this->getMessageBag();            
+        }       
+      
+}
