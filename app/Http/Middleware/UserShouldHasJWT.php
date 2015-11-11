b1fee2ff57672f80fd8a29079b6a927fb05e170b
Murat Ödünç <murat.asya@gmail.com>
2015-11-11 13:10:31 +0200

    adds new middleware

---
+++ app/Http/Middleware/UserShouldHasJWT.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class UserShouldHasJWT
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ( $this->userHasJWT() ) {
+            
+            return $next($request);          
+        }        
+        
+        return redirect()
+                ->action('Auth\AuthController@getLogin')
+                ->with('neededLogin', 'You need to refresh the session via login again to go on !');       
+    }
+    
+    
+    /**
+     * Determine if authenticated user has JWT Token for Clear Settle API
+     * 
+     * @return boolean
+     */
+    protected function userHasJWT()
+    {
+        $user = \Auth::getUser();
+        
+        if (! $user) {
+            
+            return false;
+        }
+        
+        if ($user instanceof \App\Contracts\Auth\ClearSettleEloquentPayload)  {
+            
+            
+            return $user->authHasCSJWT();
+        }
+        
+        return false;
+    }
+}
