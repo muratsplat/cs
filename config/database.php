51336615ac2924fe592a96450ed8e78f137b6956
Murat Ödünç <murat.asya@gmail.com>
2015-11-13 08:41:21 +0200

    heroku does not support SQLite

--- config/database.php
+++ config/database.php
@@ -1,5 +1,13 @@
 <?php
 
+// for Heroku DB
+$url = parse_url(getenv("DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 return [
 
     /*
@@ -66,10 +74,10 @@ return [
 
         'pgsql' => [
             'driver'   => 'pgsql',
-            'host'     => env('DB_HOST', 'localhost'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host'     => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'charset'  => 'utf8',
             'prefix'   => '',
             'schema'   => 'public',
