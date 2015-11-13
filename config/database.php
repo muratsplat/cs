401e622b663e67a1832c4b6e3a478b70bd1cc326
Murat Ödünç <murat.asya@gmail.com>
2015-11-13 08:26:30 +0200

    fixes path

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
 
         'sqlite' => [
             'driver'   => 'sqlite',
-            'database' => storage_path('local.sqlite'),
+            'database' => storage_path('../local.sqlite'),
             'prefix'   => '',
         ],
 
